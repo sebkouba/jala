c755cd4384f45d682fa4ac580f8e7f22b39a739d
Sebastian Kouba <little.sebby@gmail.com>
2015-12-15 15:10:20 +0100

    create csv, started tidying up

--- make.py
+++ make.py
@@ -1,40 +1,58 @@
 # -*- coding: utf-8 -*-
-__author__ = 'seb'
-"""
-iterate over log
-create value array for user & category in one loop
-create html out files
-"""
-
-# -*- coding: utf-8 -*-
-__author__ = 'sebastian.kouba'
-
 import re
 import os
 import pprint
 import operator
 from CategoryParser import CategoryParser
 import HtmlBuilder as hb
+import csv
 
+
+def write_csv(data, target_dir):
+    target = os.path.join(target_dir, 'user_data.csv')
+    with open(target, 'wb') as fw:
+        w = csv.writer(fw)
+        #w.writerow(data.keys())
+        for line in data:
+            w.writerow(data[line].values())
+            """w.writerow(str(data[line]["user"]) + ";" +
+                       str(data[line]["duration"]) + ";" +
+                       str(data[line]["requests"]))
+            """
+
+
+# get all access_log files in working dir
+def get_files():
+    file_list = []
+    cwd = os.getcwd()
+    for root, dirs, files in os.walk(cwd):
+        for name in files:
+            if name.startswith("access_log"):
+                file_list.append(name)
+    return file_list
+
+
+def create_data_dict():
+    # continue here
 
 line_re = re.compile(
     r'^([^\s]+)\s([^\s]+)\s([^\s]+)\s\[([^\]]+)\]\s\"(GET|POST)\s([^\s]+)\s([^\"]+)\"\s([^\s]+)\s([^\s]+)\s([^\s]+)\s\"([^\"]+)\"\s\"([^\"]+)\"\s\"([^\"]+)\"\s')
 
 #target = os.path.join("/Users/seb/dev/jala/logs", "access_log.2015-11-26")
-#target = os.path.join("C:\\", "dev", "access_log.2015-12-01")
 target = os.path.join("C:\\", "dev", "access_log.2015-11-26")
+#target = os.path.join("C:\\", "dev", "access_log.2015-12-01")
+#target = os.path.join("C:\\", "dev", "access_log.2015-12-10")
 
 user_raw_data = {}
 cat_parser = CategoryParser()
 
-user_data = []
+# user_data = []
 cat_data = []
 
 with open(target) as f:
-    case_dict = {
-
-    }
+    case_dict = {}
     for line in f:
+        # split lines into tokens using regexp
         tokens = line_re.match(line)
         if tokens:
             sourceip = tokens.group(1)
@@ -51,7 +69,7 @@ with open(target) as f:
             agent = tokens.group(12)
             session = tokens.group(13)
 
-            cat_parser.add_values(destination, duration, size)
+            # cat_parser.add_values(destination, duration, size)
 
             if request_user not in user_raw_data.keys():
                 user_raw_data[request_user] = {"user": request_user,
@@ -62,7 +80,8 @@ with open(target) as f:
 
     print ("-----------------------------------------------------------------")
     #pprint.pprint(cp.result)
-    for cat in cat_parser.result:
+    # not doing the request categories for now
+    """for cat in cat_parser.result:
         cat_data.append("{'category': '%s', "
                         "'duration': %s, "
                         "'size': %s, "
@@ -71,6 +90,7 @@ with open(target) as f:
                            cat_parser.result[cat]["duration"],
                            cat_parser.result[cat]["size"],
                            cat_parser.result[cat]["count"]))
+    """
 
     user_data = "["
     for user in user_raw_data:
@@ -80,7 +100,10 @@ with open(target) as f:
                             user_raw_data[user]["duration"],
                             user_raw_data[user]["requests"]))
     user_data += "]"
-    hb.HtmlBuilder(user_data)
+    builder = hb.HtmlBuilder(user_data)
+
+    write_csv(user_raw_data, builder.dir_name)
+
     print("done")
 
 
